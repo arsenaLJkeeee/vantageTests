f625eb7b077fd7e54c8e69793ff2b522bf4e5b4e
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-16 17:45:17 +0300

    add import

--- src/test/java/com.abbyy/vantageTestBase.java
+++ src/test/java/com.abbyy/vantageTestBase.java
@@ -1,5 +1,6 @@
 package com.abbyy;
 
+import com.abbyy.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
