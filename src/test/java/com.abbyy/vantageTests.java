320b4d982427c36362423bf4f7e68636b31b324f
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-16 18:11:44 +0300

    add click

--- src/test/java/com.abbyy/vantageTests.java
+++ src/test/java/com.abbyy/vantageTests.java
@@ -86,6 +86,9 @@ public class vantageTests extends vantageTestBase{
                     .setValue("WouldLikeToWorkInABBYYatVantageProject@gmail.com");
             $("#label-projectdescriptiontemp-f1933e9f-c6d9-4c86-a8ec-6c376b31a77d")
                     .shouldBe(visible, Duration.ofSeconds(2))
+                    .click();
+
+            $("#label-projectdescriptiontemp-f1933e9f-c6d9-4c86-a8ec-6c376b31a77d")
                     .setValue("Hello, i'm Vladimir, Quality Assurance Engineer with a demonstrated history of working in software product-based companies, and i inspired by ABBYY and would like to assure an outstanding quality in Vantage, lets have an interview!");
 
         });
